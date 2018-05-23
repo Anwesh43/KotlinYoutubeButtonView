8535a20e058628fc98a55cd54725387a12ef2084
Anwesh43 <anweshthecool0@gmail.com>
2018-05-23 08:19:06 +0530

    adding State class

--- youtubebuttonview/src/main/java/com/example/youtubebuttonview/YoutubeButtonView.kt
+++ youtubebuttonview/src/main/java/com/example/youtubebuttonview/YoutubeButtonView.kt
@@ -24,4 +24,30 @@ class YoutubeButtonView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State (var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += dir * 0.1f
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j]  = prevScale + dir
+                j += dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
