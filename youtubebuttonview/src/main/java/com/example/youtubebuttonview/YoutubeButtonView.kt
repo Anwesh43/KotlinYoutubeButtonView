93632b324a424f83db85ebbaf23d3019f3e554be
Anwesh43 <anweshthecool0@gmail.com>
2018-05-23 08:21:34 +0530

    adding Animator class

--- youtubebuttonview/src/main/java/com/example/youtubebuttonview/YoutubeButtonView.kt
+++ youtubebuttonview/src/main/java/com/example/youtubebuttonview/YoutubeButtonView.kt
@@ -50,4 +50,33 @@ class YoutubeButtonView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(updatecb : () -> Unit) {
+            if (animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
