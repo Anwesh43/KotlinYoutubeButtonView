19b8a489e85e10393e123e24ad782586f7aeec3a
Anwesh43 <anweshthecool0@gmail.com>
2018-05-23 09:23:22 +0530

    adding youtubebuttoncompletelistener in activity

--- app/src/main/java/com/example/anweshmishra/kotlinyoutubebuttonview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinyoutubebuttonview/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.anweshmishra.kotlinyoutubebuttonview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.youtubebuttonview.YoutubeButtonView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        YoutubeButtonView.create(this)
+        val view : YoutubeButtonView = YoutubeButtonView.create(this)
+        view.addYoutubeButtonListener {
+            Toast.makeText(this, "Completed", Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
