fbd16cde0eda123dad7eba463e11e6936de44c9d
Anwesh43 <anweshthecool0@gmail.com>
2018-05-23 08:13:12 +0530

    adding YoutubeButtonView class

--- youtubebuttonview/src/main/java/com/example/youtubebuttonview/YoutubeButtonView.kt
+++ youtubebuttonview/src/main/java/com/example/youtubebuttonview/YoutubeButtonView.kt
@@ -7,3 +7,21 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.*
 import android.content.*
+
+class YoutubeButtonView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
