c55b3d15215ac935633ccfe119905bb50b0ff09a
Anwesh43 <anweshthecool0@gmail.com>
2018-05-23 09:10:14 +0530

    adding view in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinyoutubebuttonview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinyoutubebuttonview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinyoutubebuttonview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.youtubebuttonview.YoutubeButtonView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        YoutubeButtonView.create(this)
     }
 }
